983614847f94195448515073ce69f2e9e17d9355
Justin Chapman <commonmodestudio@gmail.com>
2019-05-19 12:13:05 -0600

    Exercise 2.1 local tail-recursive

--- src/main/scala/fpis/Ch2.scala
+++ src/main/scala/fpis/Ch2.scala
@@ -1,9 +1,23 @@
 package fpis
 
+import annotation.tailrec
+
 object Ch2 {
-    // Exercise 2.1
-    def fib(n: Int): Int = n match {
+    // Exercise 2.1 - Naive
+    def fib1(n: Int): Int = n match {
         case 0 | 1 => n
-        case _ => fib(n - 1) + fib(n - 2)
+        case _ => fib1(n - 1) + fib1(n - 2)
     }
+
+    // Exercise 2.1 - Local tail-recursive
+    def fib2(n: Int): Int = {
+        @tailrec def fib(m: Int, x: Int, y: Int): Int = m + 2 match {
+            case `n` => x + y
+            case _ => fib(m + 1, y, x + y)
+        }
+        fib(0, 0, 1)
+    }
+
+    // Exercise 2.2
+    def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = false
 }
--- src/test/scala/fpis/Ch2Spec.scala
+++ src/test/scala/fpis/Ch2Spec.scala
@@ -2,9 +2,19 @@ import org.scalatest.FunSpec
 import fpis.Ch2
 
 class Ch2Spec extends FunSpec {
-  describe("fib") {
+  describe("fib1") {
     it("returns the nth fibonnaci number") {
-      assert(Ch2.fib(6) == 8)
+      assert(Ch2.fib1(6) == 8)
     }
+  }
+
+  describe("fib2") {
+    it("returns the nth fibonnaci number with tail call optimization") {
+      assert(Ch2.fib2(6) == 8)
+    }
+  }
+
+  describe("isSorted") {
+    it("returns whether an array is sorted via a given function") (pending)
   }
 }
